0c4308dab4484bf24a4ccad79715ab4c2dc9f628
Elmah <elmah.michal@gmail.com>
2020-02-19 14:19:36 +0100

    Update player credentials UI

--- Assets/Scripts/UI/UISettingsCredentials.cs
+++ Assets/Scripts/UI/UISettingsCredentials.cs
@@ -1,11 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class UISettingsCredentials : MonoBehaviour
 {
-    public PlayerInfo SettingsManager;
+    /*Private consts fields*/
 
-    private void OnEnable()
+    /*Private fields*/
+
+    [SerializeField]
+    private InputField InputFieldCompanyName;
+    [SerializeField]
+    private InputField InputFieldPlayerNickName;
+    [SerializeField]
+    private PlayerInfo PlayerInfoComponent;
+
+    /*Public consts fields*/
+
+    /*Public fields*/
+
+    /*Private methods*/
+
+    private void Start()
     {
-        SettingsManager.Load();
+        InputFieldCompanyName.text = PlayerInfoComponent.CompanyName;
+        InputFieldPlayerNickName.text = PlayerInfoComponent.Nickname;
+    }
+
+    /*Public methods*/
+
+    public void OnButtonSaveCliked()
+    {
+        PlayerInfoComponent.Apply(InputFieldCompanyName.text, InputFieldPlayerNickName.text);
     }
 }
