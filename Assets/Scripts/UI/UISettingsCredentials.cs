5eddf389bdc900c6086efebbc71d4907132e0386
Elmah <elmah.michal@gmail.com>
2020-08-20 20:32:53 +0200

    Update settings UI

--- Assets/Scripts/UI/UISettingsCredentials.cs
+++ Assets/Scripts/UI/UISettingsCredentials.cs
@@ -1,37 +1,70 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
+using TMPro;
 
-public class UISettingsCredentials : MonoBehaviour
+namespace ITCompanySimulation.UI
 {
-    /*Private consts fields*/
-
-    /*Private fields*/
-
-    [SerializeField]
-    private InputField InputFieldCompanyName;
-    [SerializeField]
-    private InputField InputFieldPlayerNickName;
-    [SerializeField]
-    private PlayerInfo PlayerInfoComponent;
-
-    /*Public consts fields*/
-
-    /*Public fields*/
-
-    /*Private methods*/
-
-    private void Start()
+    public class UISettingsCredentials : MonoBehaviour
     {
-        InputFieldCompanyName.text = PlayerInfoComponent.CompanyName;
-        InputFieldPlayerNickName.text = PlayerInfoComponent.Nickname;
-    }
+        /*Private consts fields*/
 
-    /*Public methods*/
+        /// <summary>
+        /// How long text should be displayed after clicking save button
+        /// </summary>
+        private const float TextCredentialsSavedDisplayTime = 2f;
 
-    public void OnButtonSaveCliked()
-    {
-        PlayerInfoComponent.Apply(InputFieldCompanyName.text, InputFieldPlayerNickName.text);
+        /*Private fields*/
+
+        [SerializeField]
+        private TMP_InputField InputFieldCompanyName;
+        [SerializeField]
+        private TMP_InputField InputFieldPlayerNickName;
+        [SerializeField]
+        private TextMeshProUGUI TextCredentialsSaved;
+        [SerializeField]
+        private PlayerInfo PlayerInfoComponent;
+        /// <summary>
+        /// How long is this text active
+        /// </summary>
+        private float TextCredentialsSavedActiveTime;
+
+        /*Public consts fields*/
+
+        /*Public fields*/
+
+        /*Private methods*/
+
+        private void Start()
+        {
+            InputFieldCompanyName.text = PlayerInfoComponent.CompanyName;
+            InputFieldPlayerNickName.text = PlayerInfoComponent.Nickname;
+            TextCredentialsSaved.gameObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (true == TextCredentialsSaved.gameObject.GetActive())
+            {
+                TextCredentialsSavedActiveTime += Time.deltaTime;
+
+                if (TextCredentialsSavedActiveTime >= TextCredentialsSavedDisplayTime)
+                {
+                    TextCredentialsSaved.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        private void OnDisable()
+        {
+            TextCredentialsSaved.gameObject.SetActive(false);
+        }
+
+        /*Public methods*/
+
+        public void OnButtonSaveCliked()
+        {
+            PlayerInfoComponent.Apply(InputFieldCompanyName.text, InputFieldPlayerNickName.text);
+            TextCredentialsSaved.gameObject.SetActive(true);
+            TextCredentialsSavedActiveTime = 0f;
+        }
     }
 }
