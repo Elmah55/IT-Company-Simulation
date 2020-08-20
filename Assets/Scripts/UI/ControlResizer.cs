6b904ebdff664f6b189b16cffff4e9a9bcbc3207
Elmah <elmah.michal@gmail.com>
2020-08-20 20:38:16 +0200

    Use IPointerEnterHandler and IPointerExitHandler interfaces instead of EventTrigger component in ControlResizer component

--- Assets/Scripts/UI/ControlResizer.cs
+++ Assets/Scripts/UI/ControlResizer.cs
@@ -5,15 +5,13 @@ using UnityEngine.EventSystems;
 /// This component will resize control by given % when pointer is above it.
 /// This can be used for selection effect
 /// </summary>
-[RequireComponent(typeof(EventTrigger))]
 [RequireComponent(typeof(RectTransform))]
-public class ControlResizer : MonoBehaviour
+public class ControlResizer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     /*Private consts fields*/
 
     /*Private fields*/
 
-    private EventTrigger ButtonEventTrigger;
     private RectTransform ButtonTransform;
     private Vector2 NormalSize = Vector2.zero;
     private bool CheckPointerPostion;
@@ -30,21 +28,6 @@ public class ControlResizer : MonoBehaviour
 
     /*Private methods*/
 
-    private void OnPointerEnter(BaseEventData data)
-    {
-        NormalSize = ButtonTransform.sizeDelta;
-        float resizeFactor = ResizePercentage / 100f;
-        float newWidth = NormalSize.x + (resizeFactor * NormalSize.x);
-        float newHeigth = NormalSize.y + (resizeFactor * NormalSize.y);
-        Vector2 newSize = new Vector2(newWidth, newHeigth);
-        ButtonTransform.sizeDelta = newSize;
-    }
-
-    private void OnPointerExit(BaseEventData data)
-    {
-        ButtonTransform.sizeDelta = NormalSize;
-    }
-
     private void OnDisable()
     {
         //Normal size will be initialized to zero at script start
@@ -56,19 +39,23 @@ public class ControlResizer : MonoBehaviour
 
     private void Start()
     {
-        ButtonEventTrigger = GetComponent<EventTrigger>();
         ButtonTransform = GetComponent<RectTransform>();
-
-        EventTrigger.Entry pointerEnter = new EventTrigger.Entry();
-        pointerEnter.eventID = EventTriggerType.PointerEnter;
-        pointerEnter.callback.AddListener(OnPointerEnter);
-        ButtonEventTrigger.triggers.Add(pointerEnter);
-
-        EventTrigger.Entry pointerExit = new EventTrigger.Entry();
-        pointerExit.eventID = EventTriggerType.PointerExit;
-        pointerExit.callback.AddListener(OnPointerExit);
-        ButtonEventTrigger.triggers.Add(pointerExit);
     }
 
     /*Public methods*/
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        NormalSize = ButtonTransform.sizeDelta;
+        float resizeFactor = ResizePercentage / 100f;
+        float newWidth = NormalSize.x + (resizeFactor * NormalSize.x);
+        float newHeigth = NormalSize.y + (resizeFactor * NormalSize.y);
+        Vector2 newSize = new Vector2(newWidth, newHeigth);
+        ButtonTransform.sizeDelta = newSize;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ButtonTransform.sizeDelta = NormalSize;
+    }
 }
