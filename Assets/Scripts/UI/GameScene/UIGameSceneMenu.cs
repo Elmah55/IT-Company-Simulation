19d2824ebf2dfc52550758677fafdf96f354b5d7
Elmah <elmah.michal@gmail.com>
2021-01-16 15:16:40 +0100

    Add possibility to exit to menu scene using button

---
+++ Assets/Scripts/UI/GameScene/UIGameSceneMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using ITCompanySimulation.UI;
+using ITCompanySimulation.Core;
+using UnityEngine.Events;
+
+public class UIGameSceneMenu : MonoBehaviour
+{
+    /*Private consts fields*/
+
+    /*Private fields*/
+
+    [SerializeField]
+    private InfoWindow InfoWindowComponent;
+    private MainGameManager GameManagerComponent;
+
+    /*Public consts fields*/
+
+    /*Public fields*/
+
+    /*Private methods*/
+
+    private void Start()
+    {
+        GameManagerComponent = GameObject.FindGameObjectWithTag("GameManager").GetComponent<MainGameManager>();
+    }
+
+    /*Public methods*/
+
+    public void OnExitToMenuButtonClick()
+    {
+        string infoWindowText = "Do you really want to exit to main menu ?";
+
+        UnityAction okAction = () =>
+        {
+            GameManagerComponent.FinishSession();
+        };
+
+        InfoWindowComponent.ShowOkCancel(infoWindowText, okAction, null);
+    }
+}
