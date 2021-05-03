d96b1b54dd833cb48548c4dbc3c3aab725d49282
Elmah <elmah.michal@gmail.com>
2021-05-03 16:14:12 +0200

    Add ControlTextFitter component

---
+++ Assets/Scripts/UI/Common/ControlTextFitter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace ITCompanySimulation.UI
+{
+    /// <summary>
+    /// Resizes control to match text size
+    /// </summary>
+    public class ControlTextFitter : MonoBehaviour
+    {
+        /*Private consts fields*/
+
+        /*Private fields*/
+
+        /*Public consts fields*/
+
+        /*Public fields*/
+
+        [Tooltip("Text that control size will match")]
+        public TextMeshProUGUI Text;
+        public bool MatchWidth;
+        public bool MatchHeight;
+
+        /*Private methods*/
+
+        private void Update()
+        {
+            RectTransform rect = (RectTransform)this.gameObject.transform;
+            float width = MatchWidth ? Text.preferredWidth : rect.sizeDelta.x;
+            float heigth = MatchHeight ? Text.preferredHeight : rect.sizeDelta.y;
+            Vector2 rectSize = new Vector2(width, heigth);
+            rect.sizeDelta = rectSize;
+        }
+
+        /*Public methods*/
+    }
+}
